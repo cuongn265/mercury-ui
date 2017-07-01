75a77f79944146ed1f1076d7833e42cdee316a4b
cuongnm265 <cuongnm265@gmail.com>
2017-07-01 23:53:11 +0700

    Add auto slide

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -32,7 +32,7 @@ export class LandingComponent implements OnInit {
     paginationClickable: true,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
-    // autoplay: 5000
+    autoplay: 3000
   };
 
   categoryPage: any;
